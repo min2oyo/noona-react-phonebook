80d64b5f9d6a0dd124f707a7eb8bde2edb22690f
min2oyo <min2u@outlook.com>
2022-11-16 14:57:10 +0900

    입력값 받기 함수 적용

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const ContactForm = () => {
+
+  // 변수
+  const [name, setName] = useState(null);               // 이름
+  const [phoneNumber, setPhoneNumber] = useState(null); // 전화번호
 
   // 출력
   return (
@@ -8,11 +13,11 @@ const ContactForm = () => {
       <Form>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>이름</Form.Label>
-          <Form.Control type="text" placeholder="이름을 입력해주세요" />
+          <Form.Control type="text" placeholder="이름을 입력해주세요" onChange={event => setName(event.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formContatct">
           <Form.Label>전화번호</Form.Label>
-          <Form.Control type="number" placeholder="전화번호를 입력해주세요" />
+          <Form.Control type="number" placeholder="전화번호를 입력해주세요" onChange={event => setPhoneNumber(event.target.value)} />
         </Form.Group>
         <Button variant="primary" type="submit">
           추가
